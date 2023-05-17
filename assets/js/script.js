eb8d0c2665a2084d3f9505aafa09a935d7630686
unknown <gmicho015@gmail.com>
2023-05-16 21:39:04 -0400

    Fixed bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,7 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+    }
 }
 
 let submitButton = document.getElementById('submit');
